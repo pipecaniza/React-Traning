b93cf021d7a1314a0de328ee58614cc6c8191928
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-02-25 22:08:03 -0500

    GoogleAuth WIP

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -18,17 +18,35 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-    }
+    };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
 
     renderAuthButton() {
         if (this.state.isSignedIn === null){
-            return <div></div>
+            return null;
         } 
         else if (this.state.isSignedIn){
-            return <div></div>
+            return (
+                <button onClick={this.onSignOutClick} className='ui red google button'>
+                    <i className='google icon' />
+                    Sign Out
+                </button>
+            );
         } 
         else {
-            return <div></div>; 
+            return (
+                <button onClick={this.onSignInClick} className='ui red google button'>
+                    <i className='google icon' />
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
