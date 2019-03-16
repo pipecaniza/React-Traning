045e8963706ef60e11ca98cc3582bad381712921
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-03-16 16:29:33 -0500

    Client and Server WIP

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import {    
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type){
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
+        case FETCH_STREAM:
+        case CREATE_STREAM:
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);        
+        default:
+            return state;
+    } 
+};
