0184c67f5e834c2b4dde89f18e0d935a99ea51c4
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-03-17 12:44:58 -0500

    Streams WIP

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -7,18 +8,42 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className='right floated content'>
+                    <button className='ui button primary'>Edit</button>
+                    <button className='ui button negative'>Delete</button>
+                </div>
+            );
+        }
+    }   
+
     renderList() {
         return this.props.streams.map( stream => {
             return (
                 <div className='item' key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className='large middle aligned icon camera' />
                     <div className='content'>
                         {stream.title}
                         <div className='description'>{stream.description}</div>
-                    </div>
+                    </div>                    
                 </div>
             );
         });
+    }
+
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to='/streams/new' className='ui button primary'>
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
     }
 
     render() {
@@ -26,13 +51,18 @@ class StreamList extends React.Component {
             <div>
                 <h2>Strams</h2>
                 <div className='ui celled list'>{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) };
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
